ad52c6576d486b28dcd42d85e133056fd3448f16
Yelps7 <antonovaira2006@gmail.com>
2024-12-26 10:31:02 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,50 +1,108 @@
 import java.io.PrintStream;
 import java.util.Scanner;
-
 public class Main {
-    // Объявляем объект класса Scanner для ввода данных
     public static Scanner in = new Scanner(System.in);
-    // Объявляем объект класса PrintStream для вывода данных
     public static PrintStream out = System.out;
-
     public static void main(String[] args) {
-        // Считывание четырёх целых чисел x, a, b, c из консоли
-        int x = in.nextInt();
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
-        // Ввод переменной счётчика k
-        int k = 0;
+        int n = in.nextInt();
+        in.nextLine();
+        String[] words = new String[n];
+        for (int i = 0; i < n; i++) {
+            words[i] = in.nextLine();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                boolean f = false;
+                if (words[j].length() > words[j + 1].length()) {
+                    f = true;
+                }
+                else if (words[j].length() == words[j + 1].length()) {
+                    for (int k = 0; k < words[j].length(); k++) {
+                        if (words[j].charAt(k) > words[j + 1].charAt(k)) {
+                            f = true;
+                            break;
+                        } else if (words[j].charAt(k) < words[j + 1].charAt(k)) {
+                            break;
+                        }
+                    }
+                }
+                if (f) {
+                    String z = words[j];
+                    words[j] = words[j + 1];
+                    words[j + 1] = z;
+                }
+            }
+        }
+        String longestWord = words[0];
+        for (int i = 1; i < n; i++) {
+            if (words[i].length() > longestWord.length()) {
+                longestWord = words[i];
+            }
+        }
+        out.println(longestWord);
 
-        // Определение поместиться ли мусор в контейнер
-        if (x >= a) {
-            // Если место в контейнере x больше или равно объему мусора a,
-            // то вычитаем место, который занимает мусор a из контейнера x.
-            // Иначе засчитываем, что мусор a нельзя поместить в контейнер x,
-            // поэтому добавляем к счётчику k 1.
-            x -= a;
-        } else {
-            k++;
+        boolean hasAnyAnagrams = false;
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                boolean hasAnagrams = false;
+                visited[i] = true;
+                for (int j = i + 1; j < n; j++) {
+                    if (!visited[j]) {
+                        if (words[i].length() == words[j].length()) {
+                            int[] charCount = new int[256];
+                            boolean areAnagrams = true;
+                            for (int k = 0; k < words[i].length(); k++) {
+                                charCount[words[i].charAt(k)]++;
+                                charCount[words[j].charAt(k)]--;
+                            }
+                            for (int k = 0; k < 256; k++) {
+                                if (charCount[k] != 0) {
+                                    areAnagrams = false;
+                                    break;
+                                }
+                            }
+                            if (areAnagrams) {
+                                if (!hasAnagrams) {
+                                    out.print(words[i] + " ");
+                                    hasAnagrams = true;
+                                }
+                                out.print(words[j] + " ");
+                                visited[j] = true;
+                            }
+                        }
+                    }
+                }
+                if (hasAnagrams) {
+                    out.println();
+                    hasAnyAnagrams = true;
+                }
+            }
         }
-        if (x >= b) {
-            // Если место в контейнере x больше или равно объему мусора b,
-            // то вычитаем место, который занимает мусор b из контейнера x.
-            // Иначе засчитываем, что мусор b нельзя поместить в контейнер x,
-            // поэтому добавляем к счётчику k 1
-            x -= b;
-        } else {
-            k++;
+        if (!hasAnyAnagrams) {
+            out.println("Нет анаграмм");
         }
-        if (x >= c) {
-            // Если место в контейнере x больше или равно объему мусора c,
-            // то вычитаем место, который занимает мусор c из контейнера x.
-            // Иначе засчитываем, что мусор c нельзя поместить в контейнер x,
-            // поэтому добавляем к счётчику k 1
-            x -= c;
-        } else {
-            k++;
+        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        for (int i = 0; i < n; i++) {
+            String newWord = "";
+            for (int j = 0; j < words[i].length(); j++) {
+                char ch = words[i].charAt(j);
+                boolean isVowel = false;
+                for (int k = 0; k < vowels.length; k++) {
+                    if (ch == vowels[k]) {
+                        isVowel = true;
+                        break;
+                    }
+                }
+                if (isVowel) {
+                    ch = (char) (ch + 1);
+                }
+                newWord += ch;
+            }
+            words[i] = newWord;
         }
-        // Выводим счётчик k
-        out.println(k);
+        for (int i = 0; i < n; i++) {
+            out.println(words[i]);
+        }
     }
 }
